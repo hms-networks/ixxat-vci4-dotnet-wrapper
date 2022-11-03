36e042f99d9130b998d53fe691dceee4a8b32a1e
Klaus Oberhofer <klob@hms-networks.de>
2022-11-03 18:39:03 +0100

    remove path to internal server from batch files

--- src/SetPath.cmd
+++ src/SetPath.cmd
@@ -1,7 +1,12 @@
+@echo off
 rem
 rem path to VCISdk
 rem
-SET VciSDKDir=\\fs-rav-win-002.hms.se\Artifacts\IndustrialCom\Drivers\VCI4\core\246\SDK
+rem VciSDKDir is set by the VCI4 installation and points to the installed VCI4 header files
+rem You can set the path to the headers by uncomment/adjust the following line
+rem SET VciSDKDir=C:\Program Files\HMS\Ixxat VCI\sdk\vci
+
+if not exist "%VciSDKDir%" echo "Error: VciSDKDir not found"
 
 REM find latest VC install directory via vswhere
 for /f "usebackq tokens=*" %%i in (`vswhere -version "[17.0,18.0)" -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
--- src/StartManaged.cmd
+++ src/StartManaged.cmd
@@ -1,3 +1,4 @@
+@echo off
 rem
 rem path to VCISdk
 rem
